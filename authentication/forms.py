1e32e62c4e88fb4a387d95ef5c884268a4aa5a77
Pierre Ragon <pragon37@outlook.fr>
2021-12-13 16:55:30 +0100

    add review models.py and a few template fixes0

--- authentication/forms.py
+++ authentication/forms.py
@@ -4,7 +4,6 @@ from django import forms
 
 
 class LoginForm(forms.Form):
-
     username = forms.CharField(max_length=63, label='Username')
     password = forms.CharField(max_length=63, widget=forms.PasswordInput, label='Password')
 
