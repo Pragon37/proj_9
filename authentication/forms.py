505de4445fb66f7b1fec23f7306b625927c23236
Pierre Ragon <pragon37@outlook.fr>
2021-12-13 08:36:54 +0100

    Added initial login source code

---
+++ authentication/forms.py
@@ -0,0 +1,15 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import  UserCreationForm
+from django import forms
+
+
+class LoginForm(forms.Form):
+
+    username = forms.CharField(max_length=63, label='Username')
+    password = forms.CharField(max_length=63, widget=forms.PasswordInput, label='Password')
+
+class SignupForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = get_user_model()
+        fields = ('username',)
+        
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout
+from . import forms
 
-# Create your views here.
+def login_page(request):
+    form = forms.LoginForm()
+    message = ''
+    if request.method == 'POST':
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password'],
+            )
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            message = 'Invalid username or password.'
+
+    return render(request, 'authentication/login.html', context={'form': form, 'message': message})
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
+def signup_page(request):
+    form = forms.SignupForm()
+    if request.method == 'POST':
+        form = forms.SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(settings.LOGIN_REDIRECT_URL)
+    return render(request, 'authentication/signup.html', context={'form': form})
